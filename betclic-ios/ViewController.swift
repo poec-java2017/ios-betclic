b9013e971b8b5951b140e52845a855ab3a159935
xylphid <anthony@periquet.net>
2017-03-24 14:14:51 +0100

    Add (again) login form

--- betclic-ios/ViewController.swift
+++ betclic-ios/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txUsername: UITextField!
+    @IBOutlet weak var txPassword: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func signIn(_ sender: Any) {
+        let username = txUsername.text
+        let password = txPassword.text
+    }
 
 }
 
